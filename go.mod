1a2a886811eb489d448f1929a8c4a1442db532f4
Yovanny Diaz <thebigyovadiaz@gmail.com>
2024-11-11 20:26:51 -0300

    Added builder pattern

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thebigyovadiaz/go-design-pattern
+
+go 1.22.3
---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thebigyovadiaz/go-design-pattern/src/decorator"
+)
+
+func main() {
+	fmt.Println("Learning Design Patterns...")
+
+	// Decorators
+	decorator.CoffeeDecorator()
+}
